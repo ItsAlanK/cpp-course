ebe5060726af3dc96d16786d14bc0fc131744e8d
ItsAlanK <alankinsella1993@gmail.com>
2023-01-26 21:04:08 +0000

    Completed challenge combining string and variables in output

--- section-4/FirstProgram/src/main.cpp
+++ section-4/FirstProgram/src/main.cpp
@@ -5,6 +5,7 @@ int main()
     int favourite_number;
     std::cout << "Enter your favourite number between 1 and 100: ";
     std::cin >> favourite_number;
-    std::cout << "Amazing, thats my favourite number too!" << std::endl;
+    std::cout << "Wow, thats my favourite number too!" << std::endl;
+    std::cout << "No really, " << favourite_number << " is my favourite number" << std::endl;
     return 0;
 }
